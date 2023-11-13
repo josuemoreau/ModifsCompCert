a368a81e9cfe76a463fbd5852c5655e37858063c
Josué Moreau <jsmr14.12@gmail.com>
2023-11-13 16:52:49 +0100

    final correction theorem for NB->ASM translation

--- driver/Compiler.v
+++ driver/Compiler.v
@@ -18,7 +18,7 @@ Require Import Coqlib Errors.
 Require Import AST Linking Smallstep.
 (** Languages (syntax and semantics). *)
 Require Ctypes Csyntax Csem Cstrategy Cexec.
-Require Syntax.
+Require Syntax NBfacts.
 Require Clight.
 Require Csharpminor.
 Require Cminor.
@@ -52,7 +52,11 @@ Require CleanupLabels.
 Require Debugvar.
 Require Stacking.
 Require Asmgen.
+(** Proof of type safety *)
+Require Safety.
 (** Proofs of semantic preservation. *)
+Require NBtoBproof.
+Require BtoCSharpMinorproof.
 Require SimplExprproof.
 Require SimplLocalsproof.
 Require Cshmgenproof.
@@ -496,4 +500,156 @@ Proof.
   { eapply link_list_compose_passes; eauto. }
   destruct H2 as (asm_program & P & Q).
   exists asm_program; split; auto. apply c_semantic_preservation; auto.
+Qed.
+
+Definition CompCert's_passes_from_Cminor :=
+    mkpass Selectionproof.match_prog
+::: mkpass RTLgenproof.match_prog
+::: mkpass (match_if Compopts.optim_tailcalls Tailcallproof.match_prog)
+::: mkpass Inliningproof.match_prog
+::: mkpass Renumberproof.match_prog
+::: mkpass (match_if Compopts.optim_constprop Constpropproof.match_prog)
+::: mkpass (match_if Compopts.optim_constprop Renumberproof.match_prog)
+::: mkpass (match_if Compopts.optim_CSE CSEproof.match_prog)
+::: mkpass (match_if Compopts.optim_redundancy Deadcodeproof.match_prog)
+::: mkpass Unusedglobproof.match_prog
+::: mkpass Allocproof.match_prog
+::: mkpass Tunnelingproof.match_prog
+::: mkpass Linearizeproof.match_prog
+::: mkpass CleanupLabelsproof.match_prog
+::: mkpass (match_if Compopts.debug Debugvarproof.match_prog)
+::: mkpass Stackingproof.match_prog
+::: mkpass Asmgenproof.match_prog
+::: pass_nil _.
+
+Definition match_prog_cminor: Cminor.program -> Asm.program -> Prop :=
+  pass_match (compose_passes CompCert's_passes_from_Cminor).
+
+Theorem transf_cminor_program_match:
+  forall p tp,
+  transf_cminor_program p = OK tp ->
+  match_prog_cminor p tp.
+Proof.
+  intros p4 tp T.
+  unfold transf_cminor_program, time in T. rewrite ! compose_print_identity in T. simpl in T.
+  destruct (Selection.sel_program p4) as [p5|e] eqn:P5; simpl in T; try discriminate.
+  destruct (RTLgen.transl_program p5) as [p6|e] eqn:P6; simpl in T; try discriminate.
+  unfold transf_rtl_program, time in T. rewrite ! compose_print_identity in T. simpl in T.
+  set (p7 := total_if optim_tailcalls Tailcall.transf_program p6) in *.
+  destruct (Inlining.transf_program p7) as [p8|e] eqn:P8; simpl in T; try discriminate.
+  set (p9 := Renumber.transf_program p8) in *.
+  set (p10 := total_if optim_constprop Constprop.transf_program p9) in *.
+  set (p11 := total_if optim_constprop Renumber.transf_program p10) in *.
+  destruct (partial_if optim_CSE CSE.transf_program p11) as [p12|e] eqn:P12; simpl in T; try discriminate.
+  destruct (partial_if optim_redundancy Deadcode.transf_program p12) as [p13|e] eqn:P13; simpl in T; try discriminate.
+  destruct (Unusedglob.transform_program p13) as [p14|e] eqn:P14; simpl in T; try discriminate.
+  destruct (Allocation.transf_program p14) as [p15|e] eqn:P15; simpl in T; try discriminate.
+  set (p16 := Tunneling.tunnel_program p15) in *.
+  destruct (Linearize.transf_program p16) as [p17|e] eqn:P17; simpl in T; try discriminate.
+  set (p18 := CleanupLabels.transf_program p17) in *.
+  destruct (partial_if debug Debugvar.transf_program p18) as [p19|e] eqn:P19; simpl in T; try discriminate.
+  destruct (Stacking.transf_program p19) as [p20|e] eqn:P20; simpl in T; try discriminate.
+  unfold match_prog; simpl.
+  exists p5; split. apply Selectionproof.transf_program_match; auto.
+  exists p6; split. apply RTLgenproof.transf_program_match; auto.
+  exists p7; split. apply total_if_match. apply Tailcallproof.transf_program_match.
+  exists p8; split. apply Inliningproof.transf_program_match; auto.
+  exists p9; split. apply Renumberproof.transf_program_match; auto.
+  exists p10; split. apply total_if_match. apply Constpropproof.transf_program_match.
+  exists p11; split. apply total_if_match. apply Renumberproof.transf_program_match.
+  exists p12; split. eapply partial_if_match; eauto. apply CSEproof.transf_program_match.
+  exists p13; split. eapply partial_if_match; eauto. apply Deadcodeproof.transf_program_match.
+  exists p14; split. apply Unusedglobproof.transf_program_match; auto.
+  exists p15; split. apply Allocproof.transf_program_match; auto.
+  exists p16; split. apply Tunnelingproof.transf_program_match.
+  exists p17; split. apply Linearizeproof.transf_program_match; auto.
+  exists p18; split. apply CleanupLabelsproof.transf_program_match; auto.
+  exists p19; split. eapply partial_if_match; eauto. apply Debugvarproof.transf_program_match.
+  exists p20; split. apply Stackingproof.transf_program_match; auto.
+  exists tp; split. apply Asmgenproof.transf_program_match; auto.
+  reflexivity.
+Qed.
+
+Theorem cminor_semantic_preservation:
+  forall p tp,
+  match_prog_cminor p tp ->
+  forward_simulation (Cminor.semantics p) (Asm.semantics tp)
+  /\ backward_simulation (Cminor.semantics p) (Asm.semantics tp).
+Proof.
+  intros p tp M. unfold match_prog_cminor, pass_match in M; simpl in M.
+  repeat DestructM. subst tp.
+  assert (F: forward_simulation (Cminor.semantics p) (Asm.semantics p17)).
+  {
+  eapply compose_forward_simulations.
+    eapply Selectionproof.transf_program_correct; eassumption.
+  eapply compose_forward_simulations.
+    eapply RTLgenproof.transf_program_correct; eassumption.
+  eapply compose_forward_simulations.
+    eapply match_if_simulation. eassumption. exact Tailcallproof.transf_program_correct.
+  eapply compose_forward_simulations.
+    eapply Inliningproof.transf_program_correct; eassumption.
+  eapply compose_forward_simulations. eapply Renumberproof.transf_program_correct; eassumption.
+  eapply compose_forward_simulations.
+    eapply match_if_simulation. eassumption. exact Constpropproof.transf_program_correct.
+  eapply compose_forward_simulations.
+    eapply match_if_simulation. eassumption. exact Renumberproof.transf_program_correct.
+  eapply compose_forward_simulations.
+    eapply match_if_simulation. eassumption. exact CSEproof.transf_program_correct.
+  eapply compose_forward_simulations.
+    eapply match_if_simulation. eassumption. exact Deadcodeproof.transf_program_correct; eassumption.
+  eapply compose_forward_simulations.
+    eapply Unusedglobproof.transf_program_correct; eassumption.
+  eapply compose_forward_simulations.
+    eapply Allocproof.transf_program_correct; eassumption.
+  eapply compose_forward_simulations.
+    eapply Tunnelingproof.transf_program_correct; eassumption.
+  eapply compose_forward_simulations.
+    eapply Linearizeproof.transf_program_correct; eassumption.
+  eapply compose_forward_simulations.
+    eapply CleanupLabelsproof.transf_program_correct; eassumption.
+  eapply compose_forward_simulations.
+    eapply match_if_simulation. eassumption. exact Debugvarproof.transf_program_correct.
+  eapply compose_forward_simulations.
+    eapply Stackingproof.transf_program_correct with (return_address_offset := Asmgenproof0.return_address_offset).
+    exact Asmgenproof.return_address_exists.
+    eassumption.
+  eapply Asmgenproof.transf_program_correct; eassumption.
+  }
+  split. auto.
+  apply forward_to_backward_simulation. assumption.
+  apply Cminor.semantics_receptive. apply Asm.semantics_determinate.
+Qed.
+
+Theorem transf_cminor_program_correct:
+  forall p tp,
+  transf_cminor_program p = OK tp ->
+  backward_simulation (Cminor.semantics p) (Asm.semantics tp).
+Proof.
+  intros p tp T.
+  eapply (proj2 (cminor_semantic_preservation p tp _)). Unshelve.
+  apply transf_cminor_program_match; auto.
+Qed.
+
+Theorem transf_nb_program_correct:
+  forall hfuncs p tp,
+  transf_nb_program hfuncs p = OK tp ->
+  backward_simulation (SemanticsNonBlocking.semantics p) (Asm.semantics tp).
+Proof.
+  intros hfuncs p tp T.
+  unfold transf_nb_program, time in T. rewrite ! compose_print_identity in T. simpl in T.
+  destruct (NBtoB.transl_program p) as [p1|] eqn:P1; simpl in T; try discriminate.
+  unfold transf_b_program, time in T. rewrite ! compose_print_identity in T. simpl in T.
+  destruct (BtoCSharpMinor.transl_program hfuncs p1) as [p2|] eqn:P2; simpl in T; try discriminate.
+  destruct (Cminorgen.transl_program p2) as [p3|e] eqn:P3; simpl in T; try discriminate.
+  apply compose_backward_simulation with (Cminor.semantics p3).
+  eapply sd_traces; eapply Asm.semantics_determinate.
+  apply forward_to_backward_simulation.
+  eapply compose_forward_simulations.
+  apply NBtoBproof.transl_program_correct; eassumption.
+  eapply compose_forward_simulations.
+  apply (BtoCSharpMinorproof.transl_program_correct hfuncs); eassumption.
+  apply Cminorgenproof.transl_program_correct. apply Cminorgenproof.transf_program_match; auto.
+  apply NBfacts.semantics_receptive.
+  apply Cminor.semantics_determinate.
+  apply transf_cminor_program_correct; eassumption.
 Qed.
